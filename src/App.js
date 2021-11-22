fb6f1864f185e7dc076f1a277fce1e0c960c3e80
Tamunotonye Adokiye Emmanuel <57795974+ksleak@users.noreply.github.com>
2021-11-22 18:39:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,13 +20,10 @@ class LambdaDemo extends Component {
   render() {
     const { loading, msg } = this.state
 
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
+          return (
+            function MyApp({ Component, pageProps }) {
+          return <Component {...pageProps} />;
+      }
     )
   }
 }
