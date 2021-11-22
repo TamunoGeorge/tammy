b0fe30324f5b9407f73f669c73fb4a17882285ab
Tamunotonye Adokiye Emmanuel <57795974+ksleak@users.noreply.github.com>
2021-11-22 18:41:43 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,47 +1,7 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import '../styles/index.css';
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-          return (
-            function MyApp({ Component, pageProps }) {
-          return <Component {...pageProps} />;
-      }
-    )
-  }
+function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />;
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
-  }
-}
-
-export default App
+export default MyApp;
